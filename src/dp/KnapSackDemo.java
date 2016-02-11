72b7771e912127541879eea96dcea5413bd51d51
Kushagra Verma <verma.k@husky.neu.edu>
2016-02-11 11:03:07 -0500

    knapsack DP

--- src/dp/KnapSackDemo.java
+++ src/dp/KnapSackDemo.java
@@ -15,12 +15,30 @@ public class KnapSackDemo {
         if (n == 0) return 0;
         if (W == 0) return 0;
 
-        if (weights[n-1] > W)
-            return knapSack(values, weights, W, n-1);
+        if (weights[n - 1] > W)
+            return knapSack(values, weights, W, n - 1);
 
         else return Math.max(
-                knapSack(values, weights, W, n-1),                              // current item is not selected
-                values[n-1] + knapSack(values, weights, W-weights[n-1], n-1)    // current item is selected
+                knapSack(values, weights, W, n - 1),                              // current item is not selected
+                values[n - 1] + knapSack(values, weights, W - weights[n - 1], n - 1)    // current item is selected
         );
+    }
+
+    private static int knapsack(int[] values, int[] weights, int W, int n) {
+
+        int[][] K = new int[n + 1][W + 1];
+
+        for (int i = 0; i < n; i++) {
+            for (int w = 0; w < W; w++) {
+
+                if (i == 0 || w == 0) K[i][w] = 0; // Base case
+                if (weights[i - 1] > w)
+                    K[i][w] = K[i - 1][w];
+                else
+                    K[i][w] = Math.max(K[i - 1][w], values[i - 1] + K[i - 1][w - weights[i - 1]]);
+            }
+        }
+
+        return K[n][W];
     }
 }
