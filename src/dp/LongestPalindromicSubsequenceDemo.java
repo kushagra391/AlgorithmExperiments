b16e6f4050c39e05a7bd5a22325ed31a62715c3f
Kushagra Verma <verma.k@husky.neu.edu>
2016-02-12 00:26:54 -0500

    base for LPS

--- src/dp/LongestPalindromicSubsequenceDemo.java
+++ src/dp/LongestPalindromicSubsequenceDemo.java
@@ -2,6 +2,13 @@ package dp;/* Authored by Kushagra on 2/12/2016. */
 
 public class LongestPalindromicSubsequenceDemo {
     public static void main(String[] args) {
-        
+
+        String S = "";
+        System.out.println(lps(S));
+
+    }
+
+    private static int lps(String S) {
+        return 0;
     }
 }
