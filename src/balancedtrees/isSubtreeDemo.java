d1654c404fe0efe5c073e8a87d72921f2ae1db05
Kushagra Verma <verma.k@husky.neu.edu>
2016-02-12 23:55:29 -0500

    worked out isSubtree

---
+++ src/balancedtrees/isSubtreeDemo.java
@@ -0,0 +1,19 @@
+package balancedtrees;/* Authored by Kushagra on 2/12/2016. */
+
+import trees.TreeNode;
+
+public class isSubtreeDemo {
+    public static void main(String[] args) {
+
+        TreeNode r = TreeNode.getBST1();
+        TreeNode s = TreeNode.getBST1();
+
+        System.out.println(isSubtree(r, s));
+    }
+
+    private static boolean isSubtree(TreeNode r, TreeNode s) {
+        
+
+        return false;
+    }
+}
