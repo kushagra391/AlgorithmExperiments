3d7294d005d886fb0007b5a9b5bcf1a6c1b39a41
Kushagra Verma <verma.k@husky.neu.edu>
2016-02-14 14:34:47 -0500

    array based permutation added

--- src/hackathon14/xStringPermutationDemo.java
+++ src/hackathon14/xStringPermutationDemo.java
@@ -6,22 +6,50 @@ public class xStringPermutationDemo {
         String S = "ABC";
         int start = 0;
         int end = S.length();
-        permute(S, start, end);
+        // permute(S, start, end - 1);
+
+
+        char[] s = S.toCharArray();
+        _permute(s, 0, s.length - 1);
+
+
     }
 
-    private static void permute(String s, int start, int end) {
+    // array based permutation
+    private static void _permute(char[] s, int start, int end) {
+
         if (start == end) {
             System.out.println(s);
         } else {
-            for (int i = start; i < end - 1; i++) {
-                swap(s, start + 1, i);
-                permute(s, i + 1, end);
-                swap(s, i, start + 1);
+            for (int i = start; i <= end; i++) {
+                _swap(s, start, i);
+                _permute(s, start + 1, end);
+                _swap(s, start, i);
             }
         }
     }
 
-    private static void swap(String s, int index1, int index2) {
+    // array based swap
+    private static void _swap(char[] s, int x, int y) {
+        char temp = s[y];
+        s[y] = s[x];
+        s[x] = temp;
+    }
+
+    // string based permutation, need to return altered string each time
+    private static void permute(String s, int start, int end) {
+        if (start == end) {
+            System.out.println(s);
+        } else {
+            for (int i = start; i <= end; i++) {
+                s = swap(s, start, i);
+                permute(s, start + 1, end);
+                s = swap(s, start, i);
+            }
+        }
+    }
+
+    private static String swap(String s, int index1, int index2) {
 
         char[] S = s.toCharArray();
 
@@ -30,6 +58,6 @@ public class xStringPermutationDemo {
         S[index1] = temp;
 
         s = new String(S);
+        return s;
     }
-
 }
